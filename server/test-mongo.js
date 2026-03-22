3e81a61e77b58e792f2955fa52a5718cd346d64f
Manish Pasula <pasulamanish0335@gmail.com>
2026-03-22 13:32:02 +0530

    Setup Render deployment configuration with MongoDB and render.yaml

--- server/test-mongo.js
+++
@@ -1,6 +0,0 @@
-const mongooseUri = process.env.MONGOOSE_URI;
-if (!mongooseUri) { console.error('Set MONGOOSE_URI'); process.exit(1); }
-const mongoose = require('mongoose');
-mongoose.connect(mongooseUri, { serverSelectionTimeoutMS: 10000 })
-  .then(() => { console.log('Mongoose connected OK'); return mongoose.disconnect(); })
-  .catch((err) => { console.error('Mongoose connect failed:', err && err.message); if (err && err.stack) console.error(err.stack); process.exit(1); });
